680fa66ee2d8595e6b097119a9b8d132206dfe68
Mikhail Romanov <miroshama@gmail.com>
2025-11-13 07:37:13 +0000

    completed point 2.1

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
+from constants import ROOMS
+from player_actions import
+from utils import
+
 def main():
     print("Первая попытка запустить проект")
+    
+
+#Переменная, ослеживающая статус игрока
+
+game_state = {
+        'player_inventory': [], # Инвентарь игрока
+        'current_room': 'entrance', # Текущая комната
+        'game_over': False, # Значения окончания игры
+        'steps_taken': 0 # Количество шагов
+  }
