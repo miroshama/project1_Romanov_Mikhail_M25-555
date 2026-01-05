9822ed707643ee8033a498dc499ae8c5efe0b44b
Mikhail <miroshama@gmail.com>
2026-01-05 21:47:05 +0300

    point 3.2 upgraded process_command

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,23 +1,68 @@
 #!/usr/bin/env python3
+# labyrinth_game/main.py
 
-from .player_actions import show_inventory, get_input
-from .utils import show_help, describe_current_room
+from .player_actions import get_input, move_player, show_inventory, take_item, use_item
+from .utils import (
+  attempt_open_treasure,
+  describe_current_room,
+  show_help,
+  solve_puzzle,
+)
+
 
 def process_command(game_state: dict, command: str) -> None:
     '''Функция для обработки пользовательских команд'''
     splited_command = command.split()
 
     match splited_command[0].lower():
+        case "look":
+            describe_current_room(game_state=game_state)
+        
+        case "use":
+            if len(splited_command) > 1:
+                use_item(game_state=game_state, item_name=splited_command[1].lower())
+            else:
+                print("Не указан предмет для использования")
+        
+        case "go":
+            if len(splited_command) > 1:
+                move_player(game_state=game_state, direction=splited_command[1].lower())
+            else:
+                print("Не указано направление движения.")
+            
+        case "take":
+            if len(splited_command) > 1:
+                take_item(game_state=game_state, item_name=splited_command[1].lower())
+            else:
+                print("Не указан предмет для поднятия.")
+            
+        case "inventory":
+            show_inventory(game_state=game_state)
+        
+        case "solve":
+            if game_state['current_room'] == "treasure_room":
+                attempt_open_treasure(game_state=game_state)
+                
+            else:
+                solve_puzzle(game_state=game_state)
+        
         case "quit":
-            game_state["game_over"] = True
+            game_state['game_over'] = True
             print("Выход из игры.")
+        
+        case "exit":
+            game_state['game_over'] = True
+            print("Выход из игры.")
+        
         case "help":
             show_help()
+        
         case _:
             if splited_command[0].lower() in ["north", "south", "east", "west"]:
                 move_player(game_state=game_state, direction=splited_command[0].lower())
             else:
                 print("Неверная команда!")
+                show_help()
 
 
 def main():
